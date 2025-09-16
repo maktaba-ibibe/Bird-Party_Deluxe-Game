b982784785bfeacea34dd5b9d990ad8167671146
David Kariuki <58103133+DkariukiM@users.noreply.github.com>
2025-09-16 20:41:20 +0300

    v0.37

---
+++ ServiceWorker.js
@@ -0,0 +1,49 @@
+const cacheName = "Ibibe-Birds Party Delux-0.37";
+const contentToCache = [
+    "Build/birdspartydelux.loader.js",
+    "Build/birdspartydelux.framework.js.br",
+    "Build/birdspartydelux.data.br",
+    "Build/birdspartydelux.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => {
+                        return name !== cacheName;
+                    })
+                    .map((name) => {
+                        console.log("[Service Worker] Deleting old cache:", name);
+                        return caches.delete(name);
+                    })
+            );
+        })
+    );
+
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
